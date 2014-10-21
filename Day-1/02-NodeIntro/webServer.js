6a1559df069ac0fa34147e4d4dd10efdf5bd3eb3
tkmagesh <tkmagesh77@gmail.com>
2014-10-21 17:40:29 +0530

    Building app server

--- Day-1/02-NodeIntro/webServer.js
+++ Day-1/02-NodeIntro/webServer.js
@@ -4,18 +4,17 @@ var http = require("http"),
 
 var onConnection = function(req,res){
     var filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
-    console.log(filePath);
     fs.exists(filePath, function(exists){
-        console.log(exists);
         if (!exists){
             res.statusCode = 404;
             res.end();
         } else {
-            var stream = fs.createReadStream(filePath, {encoding : "utf8"});
-            stream.pipe(res);
+            fs.createReadStream(filePath, {encoding : "utf8"}).pipe(res);
         }
     });
 }
 var server = http.createServer(onConnection);
 server.listen(8080);
 console.log("server listening on port 8080");
+
+/calculator?operation=add&number1=100&number=200
